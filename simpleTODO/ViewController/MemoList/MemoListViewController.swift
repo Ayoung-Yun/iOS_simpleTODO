6b76d26017608001acd6e5cd4d4cff91c843eb4b
a201903168 <ayoung.yun@deliveryhero.co.kr>
2020-01-17 16:03:21 +0900

    fix. directory

--- simpleTODO/ViewController/MemoList/MemoListViewController.swift
+++ simpleTODO/ViewController/MemoList/MemoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MemoListViewController.swift
 //  simpleTODO
 //
 //  Created by 윤아영(Yun, AYoung) on 2020/01/17.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MemoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
