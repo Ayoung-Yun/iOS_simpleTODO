85d0a564d52018d4fe11d31d0ca41b5415b6dae0
a201903168 <ayoung.yun@deliveryhero.co.kr>
2020-01-17 15:34:28 +0900

    init

---
+++ simpleTODO/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  simpleTODO
+//
+//  Created by 윤아영(Yun, AYoung) on 2020/01/17.
+//  Copyright © 2020 ayoung. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ simpleTODO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpleTODO
+//
+//  Created by 윤아영(Yun, AYoung) on 2020/01/17.
+//  Copyright © 2020 ayoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
